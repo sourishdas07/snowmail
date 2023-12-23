7e6a5fb5a6fdcdbbdc73f52aae160c254f38840d
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-12-23 02:03:40 -0500

    Changing Form Fields

--- snowmail-backend/src/data/data.controller.ts
+++ snowmail-backend/src/data/data.controller.ts
@@ -2,18 +2,27 @@ import { Controller, Get, Body } from '@nestjs/common';
 
 interface User {
   id: number;
-  name: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  school: string;
+  major: string;
+  gpa: number;
+  user: string;
 }
 
 @Controller('data')
 export class UserDataController {
+
   @Get()
   getData(): User[] {
     const users: User[] = [
-      { id: 1, name: 'John Doe' },
-      { id: 2, name: 'Jane Doe' },
-      { id: 3, name: 'Alice' },
+      // { id: 1, name: 'John Doe' },
+      // { id: 2, name: 'Jane Doe' },
+      // { id: 3, name: 'Alice' },
     ];
     return users;
   }
+
+
 }
