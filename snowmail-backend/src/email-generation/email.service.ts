7e9708fdffc9468d055f51c20d539c2714927268
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-12-28 15:04:25 -0500

    init openai api

--- snowmail-backend/src/email-generation/email.service.ts
+++ snowmail-backend/src/email-generation/email.service.ts
@@ -3,15 +3,31 @@ import { ConfigService } from '@nestjs/config';
 import { Email } from './entities/email.entity';
 import OpenAI from "openai";
 
+const { OpenAIAPI, Configuration, CreateCompletionRequest } = require('openai');
 
 @Injectable()
 export class EmailService {
+  private openai: OpenAI;
+
   constructor(
     private readonly configService: ConfigService,
   ) {
-    const openai = new OpenAI();
+    this.openai = new OpenAI({
+      organization: 'org-2J8U1tpCkIVsR1jdJVFcxwEg',
+    });
+  }
+
+  async main() {
+    const completion = await this.openai.chat.completions.create({
+      // Content Needs to be changed accordingly
+      messages: [{ role: "system", content: "You are a helpful assistant." }],
+      model: "gpt-3.5-turbo",
+    });
+
+    console.log(completion.choices[0]);
   }
 
   // TODO: Implement OpenAI API
-
 }
+
+
