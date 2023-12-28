8ac9844b526449a5f2d0772dd6c65df88e5f1f0e
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-12-28 17:32:40 -0500

    Change Styling

--- snowmail-frontend/app/home/page.tsx
+++ snowmail-frontend/app/home/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 import SignupForm from '../signup/form'
 import EmailGenerationForm from './form'
 import HomeSidebar from '@/components/Sidebar'
+import Link from 'next/link'
 
 export default async function Home() {
     const cookieStore = cookies()
@@ -17,15 +18,29 @@ export default async function Home() {
     }
 
   return (
-    <div className="grid grid-cols-6 gap-4 bg-gradient-to-tr from-white from-35% to-sky-300">
+    <div className="grid grid-cols-6 gap-4 bg-slate-100">
       <div className="col-span-1">
         <HomeSidebar />
       </div>
 
-      <div className="col-span-5 flex flex-col items-center h-full">
-        <div className="h-1/6">
+      <div className="col-span-5 flex flex-col h-full">
+
+        
+        <div className="h-1/6 flex">
           <h1 className="text-xl p-7">Welcome {user.email}!</h1>
         </div>
+
+        <div className='mt-4'>
+          <Link
+                href="/home"
+                className="py-3 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover text-sm" // Added text-sm class for smaller button size
+            >
+                Logout
+            </Link>
+        </div>
+
+
+
         <div className="h-5/6">
           {/* Email Generation Form Needs Editing */}
           <EmailGenerationForm userId={user.id}/>
